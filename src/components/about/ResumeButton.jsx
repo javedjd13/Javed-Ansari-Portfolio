ebe6f647f4ba0d986dfafa8d7438ed9118d43551
javed ansari <javed13jd@gmail.com>
2026-06-13 01:43:48 +0530

    resume link added

--- src/components/about/ResumeButton.jsx
+++ src/components/about/ResumeButton.jsx
@@ -1,6 +1,6 @@
 const ResumeButton = () => (
   <a
-    href="https://drive.google.com/file/d/1LKkGdDQgpqQlHKv-hdV9U3JVzBk4ZXFv/view?usp=sharing"
+    href="https://drive.google.com/file/d/1A7KJiY8u70rzn60xDA3ca15okTa_dmDz/view"
     target="_blank"
     rel="noopener noreferrer"
     className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
